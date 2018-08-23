34f90687cf819f87e3c6c387400071eece005e52
Alexis <alexis@monoqle.fr>
2018-08-24 00:38:21 +0200

    comments

--- NSTDemo/ViewController.swift
+++ NSTDemo/ViewController.swift
@@ -63,18 +63,17 @@ class ViewController: UIViewController {
         // Initialize the NST model
         let model = StarryNight()
         
-        // Next steps are pretty heavy,
-        // better process them on a background thread
+        // Next steps are pretty heavy, better process them on a background thread
         DispatchQueue.global().async {
             
-            // 1 - Transform image to PixelBuffer of approppriate size
+            // 1 - Transform our UIImage to a PixelBuffer of appropriate size
             guard let cvBufferInput = input.pixelBuffer(width: 720, height: 720) else {
                 print("UIImage to PixelBuffer failed")
                 completion(nil)
                 return
             }
             
-            // 2 - Feed PixelBuffer our model
+            // 2 - Feed that PixelBuffer to the model
             guard let output = try? model.prediction(inputImage: cvBufferInput) else {
                 print("Model prediction failed")
                 completion(nil)
@@ -88,8 +87,7 @@ class ViewController: UIViewController {
                 return
             }
             
-            // 4 - Resize result to the original size,
-            // then hand it back to the main thread
+            // 4 - Resize result to the original size, then hand it back to the main thread
             let finalImage = outputImage.resize(to: input.size)
             DispatchQueue.main.async {
                 completion(finalImage)
