2f88edda2cce2f171e166ffc11c043182c9392e2
Alexis Creuzot <kirualex@users.noreply.github.com>
2019-01-02 15:51:03 +0100

    Update UIImage+Resize.swift

--- NSTDemo/UIImage+Resize.swift
+++ NSTDemo/UIImage+Resize.swift
@@ -18,4 +18,27 @@ extension UIImage {
         }
         return image
     }
+    
+    // Resizeing using CoreGraphics
+    func resizeCG(size:CGSize) -> UIImage? {
+        
+        let cgImage = self.cgImage!
+
+        let destWidth = Int(size.width)
+        let destHeight = Int(size.height)
+        let bitsPerComponent = 8
+        let bytesPerPixel = cgImage.bitsPerPixel / bitsPerComponent
+        let destBytesPerRow = destWidth * bytesPerPixel
+        
+        let context = CGContext(data: nil,
+                                width: destWidth,
+                                height: destHeight,
+                                bitsPerComponent: bitsPerComponent,
+                                bytesPerRow: destBytesPerRow,
+                                space: cgImage.colorSpace!,
+                                bitmapInfo: cgImage.bitmapInfo.rawValue)!
+        context.interpolationQuality = .high
+        context.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: size))
+        return context.makeImage().flatMap { UIImage(cgImage: $0) }
+    }
 }
